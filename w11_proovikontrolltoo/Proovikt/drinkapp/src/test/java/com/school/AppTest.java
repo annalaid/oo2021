5bbb360b39fd84bbf2c49a6cfb6f16a83e4f3b70
Anna-Stiina Laidna <70900256+annalaid@users.noreply.github.com>
2021-05-06 20:49:47 +0300

    proovikt

--- w11_proovikontrolltoo/Proovikt/drinkapp/src/test/java/com/school/AppTest.java
+++ w11_proovikontrolltoo/Proovikt/drinkapp/src/test/java/com/school/AppTest.java
@@ -1,29 +1,72 @@
 package com.school;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
-import org.junit.BeforeClass;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
-public class AppTest {
+@RunWith( JUnit4.class )
+public class AppTest{
     static Bottle bottle;
     static Barrel barrel;
     static BottleBox bottlebox;
     static BottleBox secondBottlebox;
     static Drink drink;
 
-    @BeforeClass
-    public static void init(){
-        drink = new Drink("Red wine", 10.0, 0.75);
-        bottle = new Bottle(70.0, 80.0, "Glass");
-        barrel = new Barrel(10, 100, drink);
-        bottlebox = new BottleBox("Paper box", 2.0, 4.0, 6);
-        secondBottlebox = new BottleBox("Plasicbox", 20.0, 4.0, 8);
+    @Before
+    public void init(){
+        drink = new Drink( "Red wine", 10.0, 0.75 );
+        bottle = new Bottle( 70.0, 80.0, "Glass" );
+        barrel = new Barrel( 10, 100, drink );
+        bottlebox = new BottleBox( "Paper box", 2.0, 4.0, 6 );
+        secondBottlebox = new BottleBox( "Plasicbox", 20.0, 4.0, 8 );
     }
-    
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( bottle.capacity == 70.0 );
+    public void shouldAddDrinkToBottle(){
+        assertNull( bottle.getDrink() );
+        bottle.addDrink( drink );
+        assertSame( bottle.getDrink(), drink );
     }
+
+    @Test
+    public void shouldAddDrinkFromBarrelToBottle(){
+        assertNull(barrel.barrelToBottles(bottle));
+        assertEquals(141, barrel.barrelToBottles(bottle).size());;
+    }
+
+    @Test
+    public void shouldReturnEmptyBottleWeight(){
+        assertNull( bottle.getDrink() );
+        assertEquals( 35.0, bottle.bottleWeight(), 0.0 );
+    }
+
+    @Test
+    public void shouldReturnBottleSpecialPriceWithout(){
+        assertNull( bottle.getDrink() );
+        assertEquals( 80.00, bottle.bottleSpecialPrice(), 0.0 );
+    }
+
+    @Test
+    public void shouldReturnBottleSpecialPriceWithDrink(){
+        bottle.addDrink( drink );
+        assertEquals( 80.70, bottle.bottleSpecialPrice(), 0.0 );
+    }
+
+    @Test
+    public void shouldReturnBottleWeightWithDrink(){
+        bottle.addDrink( drink );
+        assertEquals( 87.50, bottle.bottleWeight(), 0.0 );
+    }
+
+    @Test
+    public void shouldNotBeAbleToFillBottleFromBarrel(){
+    }
+
+    @Test
+    public void shouldBeAbleToFill(){
+    }
+
 }
