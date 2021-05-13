ec4e09f5a6ee72b30d571e0339a519ca7ac8865b
Anna-Stiina Laidna <70900256+annalaid@users.noreply.github.com>
2021-05-13 17:45:08 +0300

    seminar

---
+++ w14_seminar/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 123;
+
+        System.out.println(x);
+        System.out.println(x / 2);
+
+
+        byte b = 100;
+        
+        System.out.println(b / 1000);
+        System.out.println(b);
+
+        short s = (short) 32768;
+        short ss = 202;
+
+        System.out.println(s);
+        System.out.println(ss);
+
+        long l = 1045678909876L;
+        long ll = -10L;
+
+        System.out.println(l);
+        System.out.println(ll);
+
+
+    }
+
+
+    
+}
